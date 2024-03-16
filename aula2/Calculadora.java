43626e4fabbbdc527d2d0e99c8f64a13ab1746bd
gabriel5illva <gabriel.osilva2023@a.ucb.br>
2024-03-16 11:13:11 -0300

    add Aula 2

---
+++ aula2/Calculadora.java
@@ -0,0 +1,23 @@
+package aula2;
+
+public class Calculadora {
+    Double numA;
+    Double numB;
+
+    Double sum(){
+        return numA + numB;
+    }
+
+    Double sub(){
+        return numA - numB;
+    }
+
+    Double mult(){
+        return numA * numB;
+    }
+
+    Double div(){
+        return numA / numB;
+    }
+
+}
---
+++ aula2/Main.java
@@ -0,0 +1,14 @@
+package aula2;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.numA = 2.0;
+        calc.numB = 3.0;
+        System.out.println("Soma: " + calc.sum());
+        System.out.println("Subtração: " + calc.sub());
+        System.out.println("Multiplicação: " + calc.mult());
+        System.out.println("Divisão: " + calc.div());
+    
+    }
+}
