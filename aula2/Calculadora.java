07c3cc225e9ae647f1836b5d9b65e379e1172983
gabriel5illva <gabriel.osilva2023@a.ucb.br>
2024-03-23 10:22:47 -0300

    add final verificar

--- aula2/Calculadora.java
+++ aula2/Calculadora.java
@@ -21,15 +21,14 @@ public class Calculadora {
     }
 
     boolean verificarNumPrimo(int n){
-
-        if(n <= 1){
+        if (n <= 1) {
             return false;
         }
-        if((n != 2) && (n % 2 == 0)){
-            return false;
+        for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
         }
-
         return true;
-
     }
 }
--- aula2/Main.java
+++ aula2/Main.java
@@ -11,6 +11,14 @@ public class Main {
         System.out.println("Divisão: " + calc.div());
         System.out.println("0: " + calc.verificarNumPrimo(0));
         System.out.println("1: " + calc.verificarNumPrimo(1));
+        System.out.println("2: " + calc.verificarNumPrimo(2));
+        System.out.println("3: " + calc.verificarNumPrimo(3));
+        System.out.println("7: " + calc.verificarNumPrimo(7));
+        System.out.println("83: " + calc.verificarNumPrimo(83));
+        System.out.println("100: " + calc.verificarNumPrimo(100));
+        System.out.println("991: " + calc.verificarNumPrimo(991));
+        System.out.println("104729: " + calc.verificarNumPrimo(104729));
+        System.out.println("14348907: " + calc.verificarNumPrimo(14348907));
     
     }
 }
