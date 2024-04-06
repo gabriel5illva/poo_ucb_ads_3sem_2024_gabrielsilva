5d314bb85148dbcf1d9ce856a0a3d8c91bde4b51
Gabriel 5illva <138382282+gabriel5illva@users.noreply.github.com>
2024-04-06 11:07:23 -0300

    corrigir sistema

--- livraria - aula 060424/Sistema.java
+++ livraria - aula 060424/Sistema.java
@@ -8,7 +8,7 @@ public class Sistema {
         
         Scanner sc = new Scanner(System.in);
         int opcao = 0;
-        Livraria livraria060424 = new Livraria
+        Livraria livraria060424 = new Livraria(); // Corrigido: Adição de parênteses para fechar a instanciação da Livraria
 
         while (opcao != 4){
 
@@ -17,6 +17,7 @@ public class Sistema {
             System.out.println("3-detalhar livro");
             System.out.println("4-sair");
             opcao = sc.nextInt();
+            sc.nextLine(); // Adicionado para consumir a quebra de linha após o nextInt()
 
             switch (opcao) {
                 case 1:
@@ -25,6 +26,7 @@ public class Sistema {
                     String nomeAutor; String anoPublicacao; String editora;
                     System.out.println("Id: ");
                     id = sc.nextInt();
+                    sc.nextLine(); // Adicionado para consumir a quebra de linha após o nextInt()
                     System.out.println("Título: ");
                     título = sc.nextLine();
                     System.out.println("Nome do autor: ");
@@ -35,21 +37,20 @@ public class Sistema {
                     editora = sc.nextLine();
 
                     Livro l = new Livro(id, título, nomeAutor, anoPublicacao, editora);
-                    Livraria.cadastrarLivro(1);
+                    livraria060424.cadastrarLivro(l); // Corrigido: Passando o objeto Livro ao método cadastrarLivro
                     break;
                 case 2:
                     System.out.println("listando livros");
-                    System.out.println(Livraria.listarLivros())
+                    System.out.println(livraria060424.listarLivros()); // Corrigido: Chamada ao método listarLivros
                     break;
 
                 case 3:
                     System.out.println("detalhando livro"); 
-                    
+                    // Adicione aqui a lógica para detalhar o livro
                     break;
 
                 case 4:
                     System.out.println("saindo");
-
                     break;
 
                 default:
