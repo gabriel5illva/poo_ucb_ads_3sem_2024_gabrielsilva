651f565e6aee16ef0d82f61e311127d991c6db08
Gabriel 5illva <138382282+gabriel5illva@users.noreply.github.com>
2024-04-27 08:23:11 -0300

    Create Banda.java
    Upload Banda

---
+++ cds/Banda.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banda extends Artista {
+    private List<String> membros;
+
+    public Banda() {
+        membros = new ArrayList<>();
+    }
+
+    public List<String> getMembros() {
+        return membros;
+    }
+
+    public void adicionarMembro(String membro) {
+        membros.add(membro);
+    }
+
+    public void removerMembro(String membro) {
+        membros.remove(membro);
+    }
+}
