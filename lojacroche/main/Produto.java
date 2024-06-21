f95e94826cffcd9975cd636e188407d44e256d11
anadiasDN <162807075+anadiasDN@users.noreply.github.com>
2024-06-21 10:53:54 -0300

    Create Produto.java

---
+++ lojacroche/main/Produto.java
@@ -0,0 +1,65 @@
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int estoque;
+    private String categoria;
+
+    public Produto(int id, String nome, String descricao, double preco, int estoque, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
